c9e98e3e0c719420d5624eed986b8f40967752eb
Allan Barbosa <allan.barbosa@yahoo.com.br>
2026-03-31 21:10:54 -0300

    Implementa associação de usuários aos veículos

--- mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -51,6 +51,7 @@ namespace mf_apis_web_services_fuel_manager.Controllers
         public async Task<ActionResult> GetById(int id)
         {
             var model = await _context.Veiculos
+                .Include(t => t.Usuarios).ThenInclude(t=> t.Usuario)
                 .Include(t => t.Consumos)
                 .FirstOrDefaultAsync(c => c.Id == id);
             if (model == null)
@@ -107,6 +108,30 @@ namespace mf_apis_web_services_fuel_manager.Controllers
                 Url.ActionLink(nameof(Create)),
                 "create", "POST"));
         }
+
+        [HttpPost ("{Id}/usuarios")]
+        public async Task<ActionResult> AddUsuario(int Id, VeiculoUsuarios model)
+        {
+            if (Id != model.VeiculoId) return BadRequest();
+            
+            _context.VeiculoUsuarios.Add(model);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetById", new { id = model.VeiculoId }, model);
+        }
+
+        [HttpDelete ("{Id}/usuarios/{usuarioId}")]
+        public async Task<ActionResult> DeleteUsuario(int Id, int usuarioId)
+        {
+            var model = await _context.VeiculoUsuarios
+                .Where(c => c.VeiculoId == Id && c.UsuarioId == usuarioId)
+                .FirstOrDefaultAsync();
+
+            if (model == null) return NotFound();
+            _context.VeiculoUsuarios.Remove(model);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
 
     }
 }
